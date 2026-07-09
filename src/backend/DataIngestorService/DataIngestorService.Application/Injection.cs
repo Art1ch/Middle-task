74f7c36587aafd28818b5c4d53bf7568ccdef49c
Artem Bogdanovich <bogdanovicha898@gmail.com>
2026-07-09 16:02:24 +0300

    Add injection of application layer

---
+++ src/backend/DataIngestorService/DataIngestorService.Application/Injection.cs
@@ -0,0 +1,35 @@
+﻿using MassTransit;
+using Microsoft.Extensions.DependencyInjection;
+using Shared.Abstractions.Events;
+
+namespace DataIngestorService.Application;
+
+public static class Injection
+{
+    public static IServiceCollection InjectApplication(this IServiceCollection services) =>
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Injection).Assembly));
+
+    private static IServiceCollection AddKafkaWithMassTransit(this IServiceCollection services)
+    {
+        services.AddMassTransit(x =>
+        {
+            x.UsingInMemory((context, cfg) =>
+            {
+                cfg.ConfigureEndpoints(context);
+            });
+
+            x.AddRider(rider =>
+            {
+                rider.AddProducer<SensorDataArrivedEvent>(
+                    "sensors-data-arrived");
+
+                rider.UsingKafka((context, k) =>
+                {
+                    //k.Host(configuration["Kafka:BootstrapServers"]);
+                });
+            });
+        });
+
+        return services;
+    }
+}
